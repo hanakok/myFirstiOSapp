b80f47fc65eaeeb618d62fb95a4618a6407a2cc9
hanako <hanako@hanakonoMacBook-puro.local>
2018-07-02 00:16:29 +0900

    my first commit

---
+++ myJanken/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  myJanken
+//
+//  Created by hanako on 2018/06/16.
+//  Copyright © 2018年 hanako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var answerImageView: UIImageView!
+}
+
